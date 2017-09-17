1b0ca7cc51ea94b67e9d225403f79f867be3135d
Bhuvanesh Rajakarthikeyan <rbhuvanesh.legends@gmail.com>
2017-09-17 10:57:47 -0500

    Final Commit
    Project Done in June 2017

---
+++ src/lockProgram/LockState.java
@@ -0,0 +1,12 @@
+package lockProgram;
+
+/**
+ *
+ Rajakarthikeyan, Bhuvanesh: 0051
+ */
+
+//Enum for storing possible types of locks that can be imposed on a data item
+public enum LockState {
+
+    RL, WL, UL
+}
---
+++ src/lockProgram/TransactionState.java
@@ -0,0 +1,13 @@
+package lockProgram;
+
+/**
+ *
+ Rajakarthikeyan, Bhuvanesh: 0051
+ */
+
+//Enum for storing possible types of states that a transaction can be in.
+public enum TransactionState {
+
+    ACTIVE,BLOCKED ,ABORTED ,COMMITTED;
+
+}
